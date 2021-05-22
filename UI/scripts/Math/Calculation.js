eee85da6886404a1c50e5712bd6dd0186a9bcd6d
agneswu47 <lanhthienlam4747@gmail.com>
2021-05-22 21:47:08 +0700

    4-0-2 2021/05/22

---
+++ UI/scripts/Math/Calculation.js
@@ -0,0 +1,39 @@
+﻿/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+//////////////////////******************///////////////////
+function addnumbers(number1, number2)
+{
+        let number3;
+    number3 = parsenInt(number1) + parseInt(number2);
+    return number3;
+}
+
+function subtractNumbers(number1, number2)
+{
+    return (number3 = number1 - number2);
+}
+
+function multiplyNumbers(number1, number2)
+{
+        let number3 = number1 * number2;
+    return (number1 * number2);
+}
+
+function devideNumbers(number1, number2)
+{
+        let number3;
+    number3 = number1 / number2;
+    return number3;
+}
+
+function powerOfNumber(number) {
+    return (number * number);
+}
+
+function getFullName(str1, str2)
+{
+        let str3;
+
+    str3 = str1+ " " + str2;
+    return str3
+}
